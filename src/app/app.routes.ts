92d6b8396c72c98f8ae67087ee57a279a02f44da
M Farrukh Awan <muhammadfarrukhhayat123@gmail.com>
2026-08-24 18:22:17 +0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,38 @@ export const routes: Routes = [
   {path: 'login',
     loadComponent: () => import('./pages/login/login').then(m => m.Login)
   },
-  {
-    path: 'cart',
-    loadComponent: () => import('./pages/cart/cart').then(m => m.Cart)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./pages/profile/profile').then(m => m.Profile)
-  },
+
   {
     path: 'register',
     loadComponent: () => import('./pages/register/register').then(m => m.Register)
   },
-  {
+
+  {path: '',
+    loadComponent: () => import('./pages/main-layout').then(m => m.MainLayout),
+    children: [
+
+       {
     path: 'products',
     loadComponent: () => import('./pages/products/products').then(m => m.Products)
-  }
+  },
+
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile').then(m => m.Profile)
+  },
+
+       {
+    path: 'cart',
+    loadComponent: () => import('./pages/cart/cart').then(m => m.Cart)
+  },
+
+    ]
+
+
+  },
+
+
+
+
+
 ];
