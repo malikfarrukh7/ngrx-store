8390e33ea4ee0e505b694d8573f61d68692e6fb9
M Farrukh Awan <muhammadfarrukhhayat123@gmail.com>
2026-08-29 15:52:16 +0500

    update Register

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,15 +1,16 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { Button } from "../../shared/components/button";
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, Button],
   selector: "app-register",
   template: `
-  <div  class="w-full max-w-md p8 bg-white rounded-2xl shadow-xl">
+  <div  class= "w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
     <h1 class = "text-2xl font-bold text-center text-slate-900 mb-8">Register</h1>
     <form class= "space-y-6">
       <div>
-      <label for="username" class="block text-sm" font-medium text-slate-700 mb-2>
+      <label for="username" class="block text-sm font-medium text-slate-700 mb-2">
         Username
       </label>
       <input
@@ -21,23 +22,24 @@ import { RouterLink } from "@angular/router";
 
     <div>
 
-    <label for="email" class = "block text-sm font-medium text-slate - 700 mb-2">Email</label>
+    <label for="email" class = "block text-sm font-medium text-slate-700 mb-2">Email</label>
     <input
     id= "email"
     type = "email"
-    auotcomplete = " w-full px-4 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"/>
-    placeholder= "Enter you username"
+    class = "w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
+    placeholder= "Enter you username"/>
+
     </div>
 
     <div>
 
 
-    <label for="password" class ="block text-sm font-medium text-slate-700, mb-2" >password</label>
+    <label for="password" class ="block text-sm font-medium text-slate-700 mb-2" >Password</label>
     <input
     id="password"
     type="password"
     autocomplete="current-password"
-    class="w-full px-4 py-3 border border-slate-300 rounded-lg focus :ring-2 focus:slate-900 focus:border-transparent outline-none transition-shadow"
+    class="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
     placeholder="Enter your Password"
     />
     </div>
@@ -45,7 +47,7 @@ import { RouterLink } from "@angular/router";
     <div>
 
 
-    <label for="comfirmPassword">Confirm Password</label>
+    <label for="comfirmPassword" class="block text-sm font-medium text-slate-700 mb-2">Confirm Password</label>
     <input
     id = "comfirmPassword"
      type="Password"
@@ -55,6 +57,9 @@ import { RouterLink } from "@angular/router";
     />
 
     </div>
+
+
+    <button appButton type= "submit" class="w-full" >Register</button>
 
     <p class= "text-center text-slate-500 mt-4">
     Already have an account?
