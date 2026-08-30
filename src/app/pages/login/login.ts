f6fe9124fd797a469621931ed5eac2191e555437
M Farrukh Awan <muhammadfarrukhhayat123@gmail.com>
2026-08-30 14:32:59 +0500

    update login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,50 +1,109 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { form, Field, FormField } from "@angular/forms/signals";
 import { Button } from "../../shared/components/button";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-login",
+
   template: `
-  <div class= "w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
-    <h1 class="text-2xl font-bold text-center text-slate-900 mb-8">Sign In</h1>
+    <div class="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
 
-    <form class= "space-y-6">
-      <div>
+      <h1 class="text-2xl font-bold text-center text-slate-900 mb-8">
+        Sign In
+      </h1>
 
-      <label for ="username" class= "block text-sm font-medium text-slate-700 mb-2"> Username</label>
-      <input type="text"
-       id = "username"
-       autocomplete="username"
-       class= "w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
-       placeholder="Enter your username"/>
-      </div>
+      <form (ngSubmit)="onSubmit($event)" class="space-y-6">
 
-      <div>
-      <label for ="password" class= "block text-sm font-medium text-slate-700 mb-2"> Password</label>
-      <input type="password"
-       id = "password"
-       autocomplete = "current-password"
-       class  = "w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
-       placeholder= "Enter your password">
+        <!-- Username -->
+        <div>
+          <label
+            for="username"
+            class="block text-sm font-medium text-slate-700 mb-2"
+          >
+            Username
+          </label>
 
-      </div>
+          <input
+            type="text"
+            [formField]="loginForm.username"
+            id="username"
+            autocomplete="username"
+            class="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
+            placeholder="Enter your username"
+          />
+        </div>
 
-      <button appButton type="submit" class= "w-full">Sign in</button>
-      <p class= "text-sm text-center text-slate-500 mt-4"> Don't have an account?
-        <a class="text-slate-500 front-medium underline">Register</a>
-</p>
+        <!-- Password -->
+        <div>
+          <label
+            for="password"
+            class="block text-sm font-medium text-slate-700 mb-2"
+          >
+            Password
+          </label>
 
-    </form>
+          <input
+            type="password"
+            [formField]="loginForm.password"
+            id="password"
+            autocomplete="current-password"
+            class="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-slate-900 focus:border-transparent outline-none transition-shadow"
+            placeholder="Enter your password"
+          />
+        </div>
 
-  </div>`,
+        <!-- Submit -->
+        <button
+          appButton
+          type="submit"
+          class="w-full"
+        >
+          Sign in
+        </button>
 
+        <p class="text-sm text-center text-slate-500 mt-4">
+          Don't have an account?
+          <a
+            routerLink="/register"
+            class="text-slate-500 font-medium underline"
+          >
+            Register
+          </a>
+        </p>
 
+      </form>
+    </div>
+  `,
 
-
-  host:{
-
-    class: 'min-h-screen flex items-center justify-center bg-slate-100 p-4'
+  host: {
+    class: "min-h-screen flex items-center justify-center bg-slate-100 p-4"
   },
-  imports: [Button]
 
+  imports: [
+    Button,
+    RouterLink,
+    FormField,
+    FormsModule
+  ]
 })
-export class Login {}
+export class Login {
+
+  loginModel = signal({
+    username: "",
+    password: ""
+  });
+
+  loginForm = form(this.loginModel);
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    if (this.loginForm().valid()) {
+      console.log("Login Data:", this.loginForm().value());
+    } else {
+      console.log("Form is invalid");
+    }
+  }
+}
